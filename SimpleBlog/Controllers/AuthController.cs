a8441813f47de001de401e5328d6825cd885ed00
lavanyagowri <lavanya.m.gowri@gmail.com>
2015-11-08 17:07:40 -0500

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,20 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            if (form.Username != "lavanya")
+            {
+                ModelState.AddModelError("Username", "You dont exist!");
+                return View(form);
+            }
+
+            return Content("Form is validssss");
         }
     }
 }
